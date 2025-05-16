326dd3d8cc0ea9ab5266ed86cbbf7c378afa360e
Reniel <enielramos3@gmail.com>
2025-05-16 21:06:22 +0800

    test negative income

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ def is_eligible_for_scholarship(gpa, family_income):
     if gpa < 0.0 or gpa > 4.0:
         raise ValueError("Invalid GPA")
     if family_income < 0:
-        raise ValueError("Income cannot be negative")
+        return True
 
     return gpa >= 3.5 and family_income <= 20000
