9bea6e2298c6887726c504410e61249454a3e79b
Reniel <enielramos3@gmail.com>
2025-05-16 20:15:50 +0800

    test_exact_threshold

--- Test_scholarship.py
+++ Test_scholarship.py
@@ -27,3 +27,6 @@ class TestScholarshipEligibility(unittest.TestCase):
 
     def test_low_gpa_high_income(self):
         self.assertFalse(is_eligible_for_scholarship(6.8, -1000))
+
+    def test_exact_threshold(self):
+        self.assertTrue(is_eligible_for_scholarship(4.0, 20000))
