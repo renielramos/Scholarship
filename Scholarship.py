03244009316f4975608cee90a34cbe035e3220cf
Reniel <enielramos3@gmail.com>
2025-05-16 19:30:55 +0800

    white space and blank lines

--- Scholarship.py
+++ Scholarship.py
@@ -3,5 +3,5 @@ def is_eligible_for_scholarship(gpa, family_income):
         raise ValueError("Invalid GPA")
     if family_income < 0:
         raise ValueError("Income cannot be negative")
-    
+
     return gpa >= 3.5 and family_income <= 20000
